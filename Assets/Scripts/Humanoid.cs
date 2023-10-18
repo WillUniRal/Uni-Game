0a594cad245bf91aead243e10fc0fae4ebd6399d
WillUniRal <22300044@bucks.ac.uk>
2023-10-18 14:30:24 +0100

    player refinements

--- Assets/Scripts/Humanoid.cs
+++ Assets/Scripts/Humanoid.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public class Humanoid : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Rigidbody2D rb;
+    [HideInInspector] public Rigidbody2D rb;
     public float speed = 3f;
     public float jumpForce = 10f;
+    [HideInInspector] public GroundCheck gc;
     void Awake() {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        gc = gameObject.GetComponentInChildren<GroundCheck>();
     }
-
-    // Update is called once per frame
     public void Move(int amount) {
         //Debug.Log(speed * amount);
         rb.velocity = new (speed * amount,rb.velocity.y);
     }
     public void Jump() {
+        if (!Grounded()) return;
         Debug.Log("Jump!");
         rb.AddForceY(jumpForce);
+    }
+    public bool Grounded()
+    {
+        return gc.isGrounded;
     }
 }
