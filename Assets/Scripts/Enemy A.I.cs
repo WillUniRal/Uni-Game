510443be4145e6d48c133631ccbcb9c435aefec7
AbdullahKD <abdullah.durrani2010@gmail.com>
2023-10-19 09:37:37 +0100

    added a new script called enemy

--- Assets/Scripts/Enemy A.I.cs
+++ Assets/Scripts/Enemy A.I.cs
@@ -2,19 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Will : MonoBehaviour
+public class Enemy : Humanoid
 {
-    private int i = 0;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("The will script has started");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        i++;
-        Debug.Log(i);
+        
     }
 }
+
+
+
