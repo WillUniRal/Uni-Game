d3eb8205430f0736e08d7f1c49ec590297a9b0ed
HamzLDN <75676416+HamzLDN@users.noreply.github.com>
2023-10-30 13:35:31 +0000

    New changes
    Fixed shooting of enemy and created a new function to convert keycode to ascii so we can use it for creating a control option for the menu

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -23,7 +23,8 @@ public class Player : Humanoid
     {
         base.Update();
         Move(input());
-        if (Input.GetKey(KeyCode.Space)) Jump();
+        if (CheckForASCIIKey(' ')) Jump();
+
         if (Input.GetMouseButtonDown(0)) Pew();
     }
     void Pew()
@@ -44,7 +45,20 @@ public class Player : Humanoid
             Die();
         }
     }
+    bool CheckForASCIIKey(int asciiValue)
+    {
+        if (Input.inputString.Length > 0)
+        {
+            char pressedKey = Input.inputString[0];
+            int pressedAscii = (int)pressedKey;
 
+            if (pressedAscii == asciiValue)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
     // Check if the player is dead and print "You Died"
     private void Die()
     {
