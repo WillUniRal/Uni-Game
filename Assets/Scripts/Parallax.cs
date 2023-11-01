2b7b8ca303417bd9f94e5138ff16aba8af4468cc
WillUniRal <22300044@bucks.ac.uk>
2023-11-01 00:57:34 +0000

    Update Parallax.cs

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -7,15 +7,9 @@ public class Parallax : MonoBehaviour
 {
     [SerializeField] private GameObject bg;
     [SerializeField] private float speed;
-    private Vector3 startPos;
     private Vector3 camOffset = Vector3.zero;
     private float xBound = 128f / 16f;
     private float yBound = 64f / 16f;
-
-    private void Start()
-    {
-        startPos = bg.transform.position;
-    }
 
     void Update()
     {
@@ -25,22 +19,14 @@ public class Parallax : MonoBehaviour
             (transform.position.y * speed) + camOffset.y,
             bg.transform.position.z
         );
-
         //Check if the background has gone beyond the x and y boundaries\\
-        float camPosOffsetX = bg.transform.localPosition.x;
-        float camPosOffsetY = bg.transform.localPosition.y;
-
-        if (xBound <= camPosOffsetX || camPosOffsetX <= -xBound)
-        {
-            camOffset -= new Vector3(camPosOffsetX, 0f, 0f);
-        }
-        if (yBound <= camPosOffsetY || camPosOffsetY <= -yBound)
-        {
-            camOffset -= new Vector3(0f, camPosOffsetY, 0f);
-        }
+        float camBgOffsetX = bg.transform.localPosition.x;
+        float camBgOffsetY = bg.transform.localPosition.y;
+        //Is the camera at the xBounds? if so move the bg so that the camera doesnt see the edge 
+        if (xBound <= Mathf.Abs(camBgOffsetX))
+            camOffset.x -= camBgOffsetX;
+        //Is the camera at the yBounds? if so move the bg so that the camera doesnt see the edge 
+        if (yBound <= Mathf.Abs(camBgOffsetY))
+            camOffset.y -= camBgOffsetY;
     }
 }
-
-
-
-
