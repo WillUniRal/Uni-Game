9ffd34a99b7dfad9660e6b52f24ac6bee322af45
Wi11ralph <147512158+WillUniRal@users.noreply.github.com>
2023-11-02 19:14:20 +0000

    Created hints for hamza

--- Assets/Scripts/Humanoid/Skeletons/FireballSkele.cs
+++ Assets/Scripts/Humanoid/Skeletons/FireballSkele.cs
@@ -13,6 +13,25 @@ public class FireballSkele : Enemy
     private float cooldownTimer = 0f;
     private bool isShooting = false;
 
+    Transform shootPoint;
+    Transform shootPointOffset;
+    private new void Start()
+    {
+        base.Start();
+
+        GameObject shootP = new GameObject();       // New GameObjects to base off of
+        GameObject shootPOffset = new GameObject(); // New GameObjects to base off of
+
+        shootP.name = "ShootPoint";
+        shootPOffset.name = "ShootPointOffset"; 
+
+        shootPoint =       Instantiate(shootP,  transform).transform;       // Make clones set the parent to this
+        shootPointOffset = Instantiate(shootPOffset, shootPoint).transform; // Make clones and set the parent to shootPoint
+
+        Destroy(shootP);       // Destroy initial
+        Destroy(shootPOffset); // Destroy initial
+
+    }
     private void Update()
     {
         EnemyUpdate();
@@ -54,16 +73,21 @@ public class FireballSkele : Enemy
         if (Player.invis) return;
         float direction = Math.Sign(transform.localScale.x);
         count++;
+        //Create some code that will rotate shootPoint and then shoot the fire balls from there
         GameObject _bullet = Instantiate(bullet);
+
+
+        /*
         _bullet.transform.position = transform.position;
         _bullet.transform.position -= new Vector3(
             -direction * 0.2f,
             0f,
             0f
         );
+        */
+
         _bullet.name = "Bullet (" + count + ")";
-        Vector3 bulletVelocity = new Vector3(direction * 20, 5, 0);
-        Rigidbody2D rb = _bullet.GetComponent<Rigidbody2D>();
-        rb.velocity = bulletVelocity;
+        
+        //Vector3 bulletVelocity = new Vector3(direction * 20, 5, 0);
     }
 }
