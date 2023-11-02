37ba5009e7906465b32b0ba6a694311cf1ac79c5
HamzLDN <75676416+HamzLDN@users.noreply.github.com>
2023-11-02 03:42:09 +0000

    Changed fireball and boneskele
    Now each weapons/bullets have their own velocity and weight

--- Assets/Scripts/Humanoid/Skeletons/BoneSkele.cs
+++ Assets/Scripts/Humanoid/Skeletons/BoneSkele.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 public class BoneSkele : Enemy
 {
-   
+    int count = 0;
     // Update is called once per frame
     private void Update() {
         EnemyUpdate(); //call the base update (Enemy) 
     }
-    
+
+    void Pew()
+    {
+        if (Player.invis) return;
+        float direction = Math.Sign(transform.localScale.x);
+        count++;
+        GameObject _bullet = Instantiate(bullet);
+        _bullet.transform.position = transform.position;
+        _bullet.transform.position -= new Vector3(
+            -direction * 0.2f,
+            0f,
+            0f
+        );
+        _bullet.name = "Bullet (" + count + ")";
+        Vector3 bulletVelocity = new Vector3(direction * 10, 20, 0);
+        Rigidbody2D rb = _bullet.GetComponent<Rigidbody2D>();
+        rb.velocity = bulletVelocity;
+    }
 }
--- Assets/Scripts/Humanoid/Skeletons/FireballSkele.cs
+++ Assets/Scripts/Humanoid/Skeletons/FireballSkele.cs
@@ -1,13 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 public class FireballSkele : Enemy
 {
-
+    int count = 0;
     // Update is called once per frame
     private void Update() {
         EnemyUpdate(); //call base enemy update
         
+    }
+
+    void Pew()
+    {
+        if (Player.invis) return;
+        float direction = Math.Sign(transform.localScale.x);
+        count++;
+        GameObject _bullet = Instantiate(bullet);
+        _bullet.transform.position = transform.position;
+        _bullet.transform.position -= new Vector3(
+            -direction * 0.2f,
+            0f,
+            0f
+        );
+        _bullet.name = "Bullet (" + count + ")";
+        Vector3 bulletVelocity = new Vector3(direction * 20, 5, 0);
+        Rigidbody2D rb = _bullet.GetComponent<Rigidbody2D>();
+        rb.velocity = bulletVelocity;
     }
 }
